35000c86d5cc121ff2039c34274e608818b4ef67
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-07-04 12:44:11 +0100

    Polls: Update Polls model and make migrations

--- polls/models.py
+++ polls/models.py
@@ -6,8 +6,11 @@ from django.db import models
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=255)
+	question_description = models.CharField(max_length=255, default='Basic Description')
 	pub_date = models.DateTimeField(auto_now_add=True)
 
+	def __str__(self):
+		return self.question_text
 # ------------------ QUESTION MODEL EXAMPLE ----------------------- #
 	# 1)  'Are you happy about django'                     "June 16, 09:58 WAT"
 	# 2)  'Are you...'  								   "June 15, 12:00 WAT"
@@ -17,3 +20,8 @@ class Choices(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_answer = models.CharField(max_length=20)
 	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.choice_answer
+# -------------------- CHOICES MODEL SAMPLE ------------------------------ #
+	# 1) 'Are you happy about django'                     (Yes)/(No)            (1)/(3)
