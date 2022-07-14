5f3a500555f968446d628b504ef72da14ad75a24
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-07-14 11:33:40 +0100

    edited the field pub_date for the Question model to be added manually and not automatically

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,8 @@ from django.utils import timezone
 class Question(models.Model):
 	question_text = models.CharField(max_length=255)
 	question_description = models.CharField(max_length=255, default='Basic Description')
-	pub_date = models.DateTimeField(auto_now_add=True)
+	# pub_date = models.DateTimeField(auto_now_add=True)
+	pub_date = models.DateTimeField('date published')
 
 	def __str__(self):
 		return self.question_text
@@ -27,7 +28,7 @@ class Question(models.Model):
 
 class Choices(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_answer = models.CharField(max_length=20)
+	choice_answer = models.CharField(max_length=200)
 	votes = models.PositiveIntegerField(default=0)
 
 	def __str__(self):
