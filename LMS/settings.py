154d6b6ef0a9d0d94413f6abbb8e7b8ca49146ee
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-07-14 11:17:53 +0100

    updated the template to show the static files

--- LMS/settings.py
+++ LMS/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig', # polls
+
+    # my added apps
+    'polls.apps.PollsConfig', 
 ]
 
 MIDDLEWARE = [
